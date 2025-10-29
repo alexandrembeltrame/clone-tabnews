9960d0f174a92c35c47b1f35cb5e70f1832c7f6b
alexandrembeltrame <mbalexandre@hotmail.com>
2025-10-29 14:40:02 -0300

    adds `lint:eslint:check` script with `ESLint`

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+import js from "@eslint/js";
+import nextPlugin from "@next/eslint-plugin-next";
+import jestPlugin from "eslint-plugin-jest";
+
+export default [
+  {
+    files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
+    ignores: ["node_modules", ".next", "dist"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    plugins: {
+      next: nextPlugin,
+      jest: jestPlugin,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...nextPlugin.configs["core-web-vitals"].rules,
+      ...jestPlugin.configs.recommended.rules,
+    },
+  },
+];
